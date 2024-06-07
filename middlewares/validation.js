c17b44e2829944501537f14376f98c9db4cc7155
PQuaresma-94 <pedromquaresma@gmail.com>
2024-06-07 13:07:24 -0700

    Add User controlers, models and routes

---
+++ middlewares/validation.js
@@ -0,0 +1,100 @@
+const { Joi, celebrate } = require("celebrate");
+const validator = require("validator");
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
+// Update Validate Custom Team Body - Need to add requirements validation
+const validateCustomTeamBody = celebrate({
+  body: Joi.object().keys({
+    teamName: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "itemName" field is 2',
+      "string.max": 'The maximum length of the "itemName" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+    teamImage: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'the "imageUrl" field must be a valid url',
+    }),
+  }),
+});
+
+const validateUserInfoBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "avatar" field must be filled in',
+      "string.uri": 'the "avatar" field must be a valid url',
+    }),
+    email: Joi.string().required().email().messages({
+      "string.empty": 'The "email" field must be filled in',
+      "string.email": 'the "email" field must be a valid email address',
+    }),
+    password: Joi.string().required().messages({
+      "string.empty": 'The "password" field must be filled in',
+    }),
+  }),
+});
+
+const validateAuthentication = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email().messages({
+      "string.empty": 'The "email" field must be filled in',
+      "string.email": 'the "email" field must be a valid email address',
+    }),
+    password: Joi.string().required().messages({
+      "string.empty": 'The "password" field must be filled in',
+    }),
+  }),
+});
+
+const validateCustomTeamId = celebrate({
+  params: Joi.object().keys({
+    itemId: Joi.string().length(24).hex().messages({
+      "string.empty": 'The "itemId" field must be filled in',
+      "string.hex": 'The "itemId" field must be a hexadecimal value',
+      "string.length": 'The "itemId" field must be 24 characters long',
+    }),
+  }),
+});
+
+const validateUserId = celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().length(24).hex().messages({
+      "string.empty": 'The "id" field must be filled in',
+      "string.hex": 'The "id" field must be a hexadecimal value',
+      "string.length": 'The "id" field must be 24 characters long',
+    }),
+  }),
+});
+
+const validateUpdateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "avatar" field must be filled in',
+      "string.uri": 'the "avatar" field must be a valid url',
+    }),
+  }),
+});
+
+module.exports = {
+  validateCustomTeamBody,
+  validateUserInfoBody,
+  validateAuthentication,
+  validateCustomTeamId,
+  validateUserId,
+  validateUpdateUser,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,30 @@
 const router = require("express").Router();
+const userRouter = require("./users");
+// Add Router for Custom Team and Characters
+const { auth } = require("../middlewares/auth");
+const {
+  validateUserInfoBody,
+  validateAuthentication,
+} = require("../middlewares/validation");
+const { NotFoundError } = require("../utils/errors/NotFoundError");
+const { createUser, login } = require("../controllers/users");
+
+// No Auth needed
+
+router.post("/signin", validateAuthentication, login);
+
+router.post("/signup", validateUserInfoBody, createUser);
+
+// Update afer creating character controller and route
+// router.use("/items", getCharacters);
+// router.use("/items", getAllCharacters);
+
+// Auth Needed
+
+router.use("/users", auth, userRouter);
+
+router.use((req, res, next) => {
+  next(new NotFoundError("Requested recource not found."));
+});
+
+module.exports = router;
